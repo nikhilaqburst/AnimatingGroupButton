4227169466c74ceeff208d7b0f8331dd8b0a7d3f
Nikhila Mohan <qbuser@qb-ipc-1080.local>
2016-12-16 08:55:47 +0530

    Minor changes

---
+++ AnimatingGroupButton/Classes/AnimatingGroupButton.swift
@@ -0,0 +1,21 @@
+//
+//  AnimatingGroupButton.swift
+//  Pods
+//
+//  Created by Nikhila Mohan on 15/12/16.
+//
+//
+
+import UIKit
+
+class AnimatingGroupButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
